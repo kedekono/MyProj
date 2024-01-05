140513a03ca8769a491dd4edf95286b2d2752a04
kedekono <137358913+kedekono@users.noreply.github.com>
2024-01-05 13:22:51 +0800

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -38,7 +38,7 @@ function predict() {
     }
 
     $.ajax({
-        url: "http://192.168.0.212:5000/Predict",
+        url: "http://192.168.230.126:5000/Predict",
         type: "POST",
         contentType: "application/json",
         data: JSON.stringify({
